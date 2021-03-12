3ec8a2d035207b94592e45372a37536d25cbc625
Nadia <71870432+nadiaaboudias@users.noreply.github.com>
2021-03-12 14:22:45 +0100

    Update jeuDeRole.sql

--- jeuDeRole.sql
+++ jeuDeRole.sql
@@ -1,11 +1,11 @@
 
-$sql = "CREATE DATABASE jeuDeRole";
+$sq1 = "CREATE DATABASE jeuDeRole";
 
 $sq2 = "CREATE TABLE users (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   login VARCHAR(30) NOT NULL,
   email VARCHAR(30) NOT NULL,
-  psswrd INT UNSIGNED NOT NULL,
+  psswrd VARCHAR(255) NOT NULL,
   age  INT UNSIGNED NOT NULL
   )";
 
@@ -13,5 +13,23 @@ $sq2 = "CREATE TABLE users (
   numero INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   message VARCHAR(255),
   login VARCHAR(255), 
-  FOREIGN KEY (login) REFERENCES users(login)
+  FOREIGN KEY (login) REFERENCES users(login),
+  gameID int(6),
+  FOREIGN KEY (gameID) REFERENCES game(gameID)
   )";
+  
+  $sq4 = "CREATE TABLE gamePlayers (
+  login VARCHAR(255), 
+  FOREIGN KEY (login) REFERENCES users(login),
+  gameID INT(6), 
+  FOREIGN KEY (gameID) REFERENCES game_bd(gameID)
+  )";
+  
+  $sq5 = "CREATE TABLE gamePlayers (
+  login VARCHAR(255), 
+  FOREIGN KEY (login) REFERENCES users(login),
+  gameID INT(6), 
+  FOREIGN KEY (gameID) REFERENCES game_bd(gameID)
+  )";
+
+  
