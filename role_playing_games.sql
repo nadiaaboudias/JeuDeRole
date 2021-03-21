6e667c6a89a5520e70a2ed93f58a72fa10ddffb7
Nadia <71870432+nadiaaboudias@users.noreply.github.com>
2021-03-21 21:15:03 +0100

    Update role_playing_games.sql

--- role_playing_games.sql
+++ role_playing_games.sql
@@ -1,35 +1,24 @@
+"CREATE DATABASE jeuDeRole";
 
-$sq1 = "CREATE DATABASE jeuDeRole";
-
-$sq2 = "CREATE TABLE users (
+"CREATE TABLE users (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   login VARCHAR(30) NOT NULL,
   email VARCHAR(30) NOT NULL,
   psswrd VARCHAR(255) NOT NULL,
-  age  INT UNSIGNED NOT NULL
+  age INT UNSIGNED NOT NULL
+  )";
+  
+  "CREATE TABLE game (
+  gameID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255)
   )";
 
-  $sq3 = "CREATE TABLE minichat (
-  numero INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  message VARCHAR(255),
+ "CREATE TABLE gamePlayers (
   login VARCHAR(255), 
   FOREIGN KEY (login) REFERENCES users(login),
-  gameID int(6),
-  FOREIGN KEY (gameID) REFERENCES game(gameID)
+  name VARCHAR(255), 
+  FOREIGN KEY (name) REFERENCES game(name)
   )";
-  
-  $sq4 = "CREATE TABLE gamePlayers (
-  login VARCHAR(255), 
-  FOREIGN KEY (login) REFERENCES users(login),
-  gameID INT(6), 
-  FOREIGN KEY (gameID) REFERENCES game_bd(gameID)
-  )";
-  
-  $sq5 = "CREATE TABLE gamePlayers (
-  login VARCHAR(255), 
-  FOREIGN KEY (login) REFERENCES users(login),
-  gameID INT(6), 
-  FOREIGN KEY (gameID) REFERENCES game_bd(gameID)
-  )";
+
 
   
